4788a91d34bca0ac98eed0fe46a81d13a8cb6eef
mahdimizabiself-rgb <mahdimizabiself@gmail.com>
2026-02-06 20:27:32 +0330

    Create web.py

---
+++ web.py
@@ -0,0 +1,18 @@
+# web.py
+from flask import Flask
+import threading
+import asyncio
+import bot
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is running"
+
+def run_bot():
+    asyncio.run(bot.main())
+
+if __name__ == "__main__":
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=10000)
