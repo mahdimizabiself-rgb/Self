aa145dee5cb392344d42a47c7ecb01ae6408e1ba
mahdimizabiself-rgb <mahdimizabiself@gmail.com>
2026-02-07 13:52:12 +0330

    Update web.py

--- web.py
+++ web.py
@@ -1,4 +1,3 @@
-# web.py
 import os
 from flask import Flask
 import threading
@@ -11,18 +10,15 @@ def home():
     return "Bot is running"
 
 def run_bot():
-    import bot
-    asyncio.run(bot.main())
+    from bot import main   # 👈 فقط این خط عوض شده
+    asyncio.run(main())    # 👈 این هم
 
 if __name__ == "__main__":
-    # 🔴 این خط کلید حل مشکل Render هست
     port = int(os.environ.get("PORT", 10000))
 
-    # ✅ اول Flask رو اجرا کن (PORT فوراً bind میشه)
     threading.Thread(
         target=lambda: app.run(host="0.0.0.0", port=port),
         daemon=True
     ).start()
 
-    # ✅ بعدش ربات تلگرام رو اجرا کن
     run_bot()
